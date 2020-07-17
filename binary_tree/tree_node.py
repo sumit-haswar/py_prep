55d10900393fdb07f3747c68b54ada9fb35ded7d
Sumit Haswar <sumithaswar@gmail.com>
2020-07-16 21:49:56 -0700

    binary tree problems + update readme (#6)

--- binary_tree/tree_node.py
+++ binary_tree/tree_node.py
@@ -3,5 +3,6 @@ class TreeNode:
 
     def __init__(self, data=None, left=None, right=None):
         self.data = data
+        self.val = data
         self.left = left
         self.right = right
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from linked_list import linked_list_problems
+from binary_tree import binary_tree_problems
 
 
-linked_list_problems.main()
+if __name__ == '__main__':
+    # linked_list_problems.main()
+    binary_tree_problems.main()
