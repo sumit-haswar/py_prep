54a526102c1a7fb7877f42f017f21177dddeaf0b
Sumit Haswar <sumithaswar@gmail.com>
2020-08-25 00:50:08 -0700

    add quick and merge sort

--- sort_algos.py
+++ sort_algos.py
@@ -3,11 +3,6 @@
 """
 from heap import Heap
 
-#  --- comparison sorts ---
-# bubble sort
-def bubble_sort(list):
-    return list
-
 
 # selection sort
 def selection_sort(list):
@@ -26,23 +21,104 @@ def selection_sort(list):
 
 # insertion sort
 def insertion_sort(list):
-    pass
+    # i moves left to right starting with idx = 1
+    size = len(list)
+    for i in range(1, size):
+        j = i
+        # j goes right to left
+        while j > 0 and list[j] < list[j - 1]:
+            _swap(list, j, j - 1)
+            j = j - 1
 
 
 # quick sort
-def quick_sort(list):
-    pass
+def quick_sort(list, left, right):
+    pivot = _partition(list, left, right)
+
+    if left < (pivot - 1):
+        quick_sort(list, left, pivot - 1)
+
+    if right > (pivot + 1):
+        quick_sort(list, pivot + 1, right)
+
+
+def _partition(list, left, right):
+    # start with right as pivot-val
+    pivot_value = list[right]
+
+    left_idx = left - 1
+
+    right_idx = left
+    while right_idx <= right - 1:
+        # if you find elem <= pivot_value increment left_idx swap with loop index and
+        if list[right_idx] <= pivot_value:
+            left_idx += 1
+            _swap(list, left_idx, right_idx)
+        right_idx += 1
+    # finally exchange left_idx + 1 with right
+    _swap(list, left_idx + 1, right)
+
+    # return the correct index of pivot_val
+    return left_idx + 1
 
 
 # merge sort
-def merge_sort(list):
-    pass
+def merge_sort(list, left, right):
+    if left >= right:
+        return
+
+    mid = (right + left) // 2
+
+    merge_sort(list, left, mid)
+    merge_sort(list, mid + 1, right)
+
+    _merge(list, left, mid, right)
+
+
+def _merge(list, left, mid, right):
+    left_size = mid - left + 1
+    right_size = right - mid
+
+    left_list = []
+    right_list = []
+    # copy list elements to left and right
+    for i in range(0, left_size):
+        left_list.append(list[left + i])
+
+    for j in range(0, right_size):
+        right_list.append(list[mid + 1 + j])
+
+    # perform merge onto list in lock-steps
+    left_idx = 0
+    right_idx = 0
+    idx = left
+
+    while left_idx < left_size and right_idx < right_size:
+        if left_list[left_idx] < right_list[right_idx]:
+            list[idx] = left_list[left_idx]
+            idx += 1
+            left_idx += 1
+        elif left_list[left_idx] > right_list[right_idx]:
+            list[idx] = right_list[right_idx]
+            idx += 1
+            right_idx += 1
+
+    while left_idx < left_size:
+        list[idx] = left_list[left_idx]
+        idx += 1
+        left_idx += 1
+
+    while right_idx < right_size:
+        list[idx] = right_list[right_idx]
+        idx += 1
+        right_idx += 1
 
 
 # heap sort
 def heap_sort(list):
     heap = Heap(list)
     return heap.heap_sort()
+
 
 # -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
@@ -71,11 +147,7 @@ def _get_min(list, start, end):
     return min, min_idx
 
 
-def main():
-    input = [5, 2, 15, 1, 90]
-    selection_sort(input)
-    print(input)
-
-
-if __name__ == '__main__':
-    main()
+def _swap(list, idx_a, idx_b):
+    temp = list[idx_a]
+    list[idx_a] = list[idx_b]
+    list[idx_b] = temp
--- tests/sort_algos_test.py
+++ tests/sort_algos_test.py
@@ -1,4 +1,9 @@
-from sort_algos import bubble_sort, selection_sort, heap_sort
+from sort_algos import bubble_sort, \
+    selection_sort, \
+    heap_sort, \
+    insertion_sort, \
+    quick_sort, \
+    merge_sort
 from random import randint
 
 import unittest
@@ -6,30 +11,35 @@ import unittest
 
 class SortAlgosTest(unittest.TestCase):
 
-    def test_bubble_sort(self):
-        pass
-
     def test_insertion_sort(self):
-        pass
+        input, expected = self._get_input_expected_list()
+        insertion_sort(input)
+        self.assertListEqual(expected, input)
 
     def test_selection_sort(self):
-        input, expected = self._get_input_expected_list()
-        selection_sort(input)
-        self.assertListEqual(input, expected)
+        for count in range(1, 10):
+            input, expected = self._get_input_expected_list(count)
+            selection_sort(input)
+            self.assertListEqual(expected, input)
 
     def test_quick_sort(self):
-        pass
+        for count in range(1,25):
+            input, expected = self._get_input_expected_list(count)
+            quick_sort(input, 0, len(input) - 1)
+            self.assertListEqual(expected, input)
 
     def test_merge_sort(self):
-        pass
+        input, expected = self._get_input_expected_list(16)
+        merge_sort(input, 0, len(input) - 1)
+        self.assertListEqual(expected, input)
 
     def test_heap_sort(self):
         input, expected = self._get_input_expected_list()
         input = heap_sort(input)
         self.assertListEqual(input, expected)
 
-    def _get_input_expected_list(self):
-        random_list = self._get_random_list(count=15)
+    def _get_input_expected_list(self, count=10):
+        random_list = self._get_random_list(count=count)
         expected = random_list.copy()
         expected.sort()
 
