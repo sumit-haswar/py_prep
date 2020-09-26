11f80d5b581608a0da4c90ca3a42927fa1a9695e
Sumit Haswar <sumithaswar@gmail.com>
2020-09-26 16:13:32 -0700

    add all pending algos except 8.4

--- stacks_queues/epi_stacks_queues.py
+++ stacks_queues/epi_stacks_queues.py
@@ -1,4 +1,6 @@
-import typing
+from typing import List, Iterator
+from binary_tree import TreeNode
+from collections import deque
 
 
 # -- stacks --
@@ -45,32 +47,135 @@ def evaluate_rpn_expression(exp: str) -> int:
 
 #   8.3 is a string well-formed
 def is_string_well_formed(s: str):
-    pass
+    look_up = {'}': '{', ']': '[', ')': '('}
+    stack = []
+    for ch in s:
+        if ch not in look_up:
+            stack.append(ch)
+        else:
+            top = stack.pop()
+            if top != look_up[ch]:
+                return False
+
+    return not stack
 
 
-#   8.4 normalize path-names
+# todo  8.4 normalize path-names
 
 #   (8.5) compute buildings with sunset-view
+def get_sunset_view(itr: Iterator[int]) -> List[int]:
+    """
+    Input stream is in east to west direction
+    :param itr:
+    :return:
+    """
+    stack = []
+    for idx, building in enumerate(itr):
+        if stack:
+            top = stack[-1]
+            while building > top['height']:
+                stack.pop()
+                if not stack:
+                    break
+                top = stack[-1]
+
+        stack.append({'idx': idx, 'height': building})
+
+    return [elem['idx'] for elem in reversed(stack)]
+
 
 # -- -- -- -- queues -- -- -- --
 
 #   8.6 compute binary tree nodes in order of inc. depth
+def get_tree_nodes_by_level(root: TreeNode) -> List[List[int]]:
+    curr = deque()
+    next = deque()
+    curr.append(root)
+    result = []
+    curr_level = []
+    while curr:
+        node = curr.popleft()
+        curr_level.append(node.data)
+        if node.left:
+            next.append(node.left)
+        if node.right:
+            next.append(node.right)
+
+        # current level is done
+        if not curr:
+            result.append(curr_level)
+            curr_level = []
+            # swap curr and next queues
+            temp = next
+            next = curr
+            curr = temp
+
+    return result
+
 
 #   8.7 implement a circular queue
+class ArrayQueue():
+
+    def __init__(self, capacity=10):
+        self._q = [0] * capacity
+        self.head = self.tail = self.count = 0
+
+    def size(self):
+        return self.count
+
+    def enqueue(self, elem):
+        if len(self._q) == self.count:  # resize
+            self._q = (self._q[self.head:] + self._q[:self.head])
+            self.head, self.tail = 0, self.count
+            self._q = self._q + [0] * (len(self._q) * 2 - len(self._q))
+
+        self._q[self.tail] = elem
+        self.tail = (self.tail + 1) % len(self._q)
+        self.count += 1
+
+    def dequeue(self) -> int:
+        # always dequeue from head and decrement
+        result = self._q[self.head]
+        self.head = (self.head + 1) % len(self._q)
+        self.count -= 1
+        return result
+
 
 #   8.8 implement a queue using stack
+class StackQueue():
+
+    def __init__(self):
+        self._en = []
+        self._de = []
+
+    def enqueue(self, elem):
+        self._en.append(elem)
+
+    def dequeue(self):
+        if not self._de:
+            while self._en:
+                self._de.append(self._en.pop())
+        return self._de.pop()
+
 
 #   (8.9) implement a queue with max api
 class MaxQueue():
 
     def __init__(self):
-        pass
+        self._q = deque()
+        self._dq = deque()
 
     def enqueue(self, val: int) -> None:
-        pass
+        self._q.append(val)
+        while self._dq and (val > self._dq[-1]):
+            self._dq.pop()
+        self._dq.append(val)
 
-    def dequeue(self, val: int) -> int:
-        pass
+    def dequeue(self) -> int:
+        result = self._q.popleft()
+        if self._dq[0] == result:
+            self._dq.popleft()
+        return result
 
     def get_max(self) -> int:
-        pass
+        return self._dq[0]
--- tests/stacks_queues/epi_stacks_queues_test.py
+++ tests/stacks_queues/epi_stacks_queues_test.py
@@ -2,7 +2,14 @@ import unittest
 from stacks_queues import \
     MaxStack, \
     evaluate_rpn_expression, \
-    is_string_well_formed
+    is_string_well_formed, \
+    get_sunset_view, \
+    get_tree_nodes_by_level, \
+    ArrayQueue, \
+    StackQueue, \
+    MaxQueue
+
+from binary_tree.util import build_1_to_10_bst
 
 
 class EpiStacksQueuesTestCase(unittest.TestCase):
@@ -35,8 +42,70 @@ class EpiStacksQueuesTestCase(unittest.TestCase):
         self.assertEqual(22, result)
 
     def test_is_string_well_formed(self):
-        s = ''
-        actual = is_string_well_formed(s)
+        self.assertTrue(is_string_well_formed(''))
+        self.assertTrue(is_string_well_formed('[]{}(){[()]}'))
+        self.assertFalse(is_string_well_formed('[]{}(){[(]}'))
+
+    def test_get_sunset_view(self):
+        actual = get_sunset_view(iter([6, 9, 3, 9, 5, 16, 9, 13]))
+        self.assertEqual([7, 5], actual)
+        actual = get_sunset_view(iter([17, 15, 3, 15, 32, 14, 21, 26, 8, 34,
+                                       42, 14, 5, 4, 13, 42, 19, 36, 30, 14,
+                                       45, 20, 43, 13]))
+        self.assertEqual([23, 22, 20], actual)
+
+    def test_get_tree_nodes_by_level(self):
+        tree = build_1_to_10_bst()
+        level_list = get_tree_nodes_by_level(tree)
+        self.assertListEqual([[5], [3, 9], [2, 4, 7, 10], [1, 6, 8]], level_list)
+
+    def test_stack_queue(self):
+        sq = StackQueue()
+        for e in [11, 22, 33]:
+            sq.enqueue(e)
+        self.assertEqual(11, sq.dequeue())
+        self.assertEqual(22, sq.dequeue())
+        for e in [44, 55, 66]:
+            sq.enqueue(e)
+        self.assertEqual(33, sq.dequeue())
+        self.assertEqual(44, sq.dequeue())
+        self.assertEqual(55, sq.dequeue())
+
+    def test_array_queue(self):
+        sq = ArrayQueue(8)
+        for e in range(1, 6):
+            sq.enqueue(e * 11)
+        self.assertEqual(5, sq.size())
+        for e in range(1, 6):
+            self.assertEqual(e * 11, sq.dequeue())
+
+        sq = ArrayQueue(5)
+        for e in range(1,10):
+            sq.enqueue(e * 11)
+
+        for e in range(1,10):
+            self.assertEqual(e * 11, sq.dequeue())
+
+    def test_max_queue(self):
+        mq = MaxQueue()
+        mq.enqueue(-202)
+        self.assertEqual(-202, mq.get_max())
+        mq.enqueue(131)
+        mq.enqueue(-105)
+        self.assertEqual(131, mq.get_max())
+        mq.enqueue(265)
+        mq.enqueue(-284)
+        mq.enqueue(117)
+        self.assertEqual(-202, mq.dequeue())
+        self.assertEqual(265, mq.get_max())
+        mq.enqueue(574)
+        self.assertEqual(574, mq.get_max())
+        mq.enqueue(947)
+        self.assertEqual(131, mq.dequeue())
+        self.assertEqual(947, mq.get_max())
+        self.assertEqual(-105, mq.dequeue())
+        self.assertEqual(265, mq.dequeue())
+
 
 
 if __name__ == '__main__':
