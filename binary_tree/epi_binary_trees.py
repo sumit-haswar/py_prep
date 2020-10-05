f2d36e13e8678a25c5b0f9a04a716ffd815a08a3
Sumit Haswar <sumithaswar@gmail.com>
2020-10-04 22:31:14 -0700

    implement 9.5, .8, .9, .11 (#30)

--- binary_tree/epi_binary_trees.py
+++ binary_tree/epi_binary_trees.py
@@ -1,4 +1,5 @@
 import typing
+from typing import List
 from .tree_node import TreeNode
 
 
@@ -95,7 +96,18 @@ def get_parent_pointer_lca(node_a: TreeNode, node_b: TreeNode) -> bool:
 
 #   9.5 root to leaf path sum
 def get_path_sum(root: TreeNode):
-    pass
+    def _get_path_sum(node: TreeNode, path_sum):
+
+        if node is None:
+            return 0
+
+        node_sum = (path_sum << 1) + node.data
+        if not node.left and not node.right:
+            return node_sum
+
+        return _get_path_sum(node.left, node_sum) + _get_path_sum(node.right, node_sum)
+
+    return _get_path_sum(root, 0)
 
 
 #   9.6 find root to leaf path with specified sum
@@ -125,19 +137,96 @@ def get_path_with_sum(root: TreeNode, sum: int):
     return result[1] if result[0] else ''
 
 
-#   9.7 inorder traversal without recursion
-
 #   9.8 kth node in an inorder traversal
+def get_kth_node(root: TreeNode, k: int) -> TreeNode:
+    curr = root
+    while curr:
+        left_count = curr.left.count if curr.left else 0
+        if left_count + 1 == k:
+            # this is the node
+            return curr
+        elif left_count >= k:
+            curr = curr.left
+        else:
+            curr = curr.right
+            k = k - left_count - 1
+
+    return None
+
 
 #   9.9 compute the successor (node with parents)
+def get_successor(node: TreeNode) -> TreeNode:
+    if node.right:
+        # return the leftmost child of right
+        curr = node.right
+        while curr.left:
+            curr = curr.left
+        return curr
+    curr = node
+    while curr:
+        if curr.parent and curr.parent.left is curr:
+            return curr.parent
+        curr = curr.parent
+
+    return None
 
 #   9.10 inorder traversal with constant space (Morris Traversal)
+def inorder_traversal(root: TreeNode) -> List[int]:
+    pass
 
-#   9.11 reconstruct a binary tree with traversal data (inorder and pre/postorder)
 
-#   9.12 reconstruct a binary tree from preorder traversal with markers
+#   9.11 reconstruct a binary tree with traversal data
+#   (inorder and pre/postorder)
+def reconstruct_bt(inorder: List[int], preorder: List[int]) -> TreeNode:
+    # generate inorder-map
+    inorder_map = {}
+    for idx, elem in enumerate(inorder):
+        inorder_map[elem] = idx
 
-#   (9.15) compute the right sibling tree
+    def _reconstruct_bt(in_start, in_end, pre_start, pre_end):
+
+        if in_start > in_end:
+            return None
+
+        if in_start == in_end:
+            return TreeNode(inorder[in_start])
+
+        node_data = preorder[pre_start]
+        inorder_idx = inorder_map[node_data]
+
+        left_node_count = inorder_idx - in_start
+
+        left_in_start = in_start
+        left_in_end = inorder_idx - 1
+        left_pre_start = pre_start + 1
+        left_pre_end = pre_start + left_node_count
+
+        right_in_start = inorder_idx + 1
+        right_in_end = in_end
+        right_pre_start = left_pre_end + 1
+        right_pre_end = pre_end
+
+        return TreeNode(node_data,
+                        _reconstruct_bt(left_in_start,
+                                        left_in_end,
+                                        left_pre_start,
+                                        left_pre_end),
+                        _reconstruct_bt(right_in_start,
+                                        right_in_end,
+                                        right_pre_start,
+                                        right_pre_end))
+
+    return _reconstruct_bt(0, len(inorder) - 1, 0, len(preorder) - 1)
+
+    #   (9.15) compute the right sibling tree
+
+
+def compute_right_sibling_tree():
+    pass
+
+
+# todo  9.7 inorder traversal without recursion
+# todo  9.12 reconstruct a binary tree from preorder traversal with markers
 
 
 def _get_height(node: TreeNode):
--- binary_tree/tree_node.py
+++ binary_tree/tree_node.py
@@ -1,14 +1,18 @@
-
 class TreeNode:
 
-    def __init__(self, data=None, left=None, right=None, parent=None):
+    def __init__(self,
+                 data=None,
+                 left=None,
+                 right=None,
+                 parent=None):
         self.data = data
         self.val = data
         self.left = left
         self.right = right
         self.parent = parent
+        self.count = 1
 
     def __str__(self):
         return '{} <-- {} --> {}'.format(self.left.data if self.left else 'None',
-                                        self.data,
-                                        self.right.data if self.right else 'None')
+                                         self.data,
+                                         self.right.data if self.right else 'None')
