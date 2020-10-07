b40c5fcd598c4372b25dc0821e3c725aa894870c
Sumit Haswar <sumithaswar@gmail.com>
2020-10-06 23:03:56 -0700

    initial hash tables commit

---
+++ hash_tables/epi_hash_tables.py
@@ -0,0 +1,20 @@
+import typing
+from typing import List
+
+
+def find_anagrams(list: List[str]):
+    pass
+
+
+class ContactList:
+    pass
+
+
+#   12.1
+#   12.2
+#   12.3
+#   12.4
+#   12.5
+#   12.6
+
+#   12.9
