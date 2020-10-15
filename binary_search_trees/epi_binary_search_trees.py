63dddbda40507d7614cc880766857f4f33e86cfe
Sumit Haswar <sumithaswar@gmail.com>
2020-10-14 22:26:00 -0700

    add initial bst module (#37)

---
+++ binary_search_trees/epi_binary_search_trees.py
@@ -0,0 +1,27 @@
+import typing
+from typing import List
+from binary_tree import TreeNode
+
+#   14.1 test if bt satisfies the bst property
+
+#   14.2 find the first greater than a given value in a bst
+
+#   14.3 find kth largest element in a bst
+
+#   14.4 compute the lca in a bst
+
+#   14.5 reconstruct a bst from traversal data
+
+#   14.6 find the closest entries in 3 sorted arrays
+
+#   14.7 enumerate extended integers
+
+#   14.8 build a min height bst from a sorted array
+
+#   (14.9) test if three bst nodes are totally ordered
+
+#   (14.10) the range lookup problem
+
+#   ____________________  augmented BSTs ____________________
+
+#   (14.11) add credits
