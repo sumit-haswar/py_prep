613438307dc01fccf5949078fd7e5810079a99ad
Sumit Haswar <sumithaswar@gmail.com>
2020-11-01 15:58:21 -0800

    implement all pending algos except 13.5 and 13.10

--- sorting/epi_sorting.py
+++ sorting/epi_sorting.py
@@ -1,5 +1,8 @@
-from typing import List
+from typing import List, Tuple
 import bisect
+from linked_list.epi_linked_lists import merge_sorted_list
+from linked_list import Node
+
 
 # boot-camp
 class Student():
@@ -22,7 +25,6 @@ def sort_students_by_name(students: List[Student]):
 
 #   13.1 compute intersection of two sorted arrays
 def intersect_two_sorted_lists_binary_search(arr1: List[int], arr2: List[int]) -> List[int]:
-
     def _find(elem: int, arr: List[int]):
 
         idx = bisect.bisect_left(arr, elem, 0, len(arr))
@@ -58,14 +60,14 @@ def intersect_two_sorted_lists(arr_a: List[int], arr_b: List[int]) -> List[int]:
 
         elif arr_a[idx_a] < arr_b[idx_b]:
             idx_a += 1
-        else:   # a > b
+        else:  # a > b
             idx_b += 1
 
     return result
 
 
 #   13.2 merge two sorted arrays
-def merge_sorted_arrays(a: List[int], a_count , b: List[int], b_count) -> List[int]:
+def merge_sorted_arrays(a: List[int], a_count, b: List[int], b_count) -> List[int]:
     write_idx = a_count + b_count - 1
     read_idx_b = b_count - 1
     read_idx_a = a_count - 1
@@ -77,7 +79,7 @@ def merge_sorted_arrays(a: List[int], a_count , b: List[int], b_count) -> List[i
         elif a[read_idx_a] < b[read_idx_b]:
             a[write_idx] = b[read_idx_b]
             read_idx_b -= 1
-        else: #a[read_idx_a] > b[read_idx_b]:
+        else:  # a[read_idx_a] > b[read_idx_b]:
             a[write_idx] = a[read_idx_a]
             read_idx_a -= 1
 
@@ -93,19 +95,224 @@ def merge_sorted_arrays(a: List[int], a_count , b: List[int], b_count) -> List[i
 
 #   13.3 computing the h-index
 def get_h_index(citations: List[int]) -> int:
-    pass
+    citations.sort()
+    for idx, citation in enumerate(citations):
+        right = len(citations) - idx
+        if citation >= right:
+            return len(citations[idx:])
+
+    return 0
+
 
 #   13.4 remove first name duplicates
+class Name():
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def __eq__(self, other):
+        return self.first == other.first
+
+    def __lt__(self, other):
+        return (self.last < other.last) \
+            if self.first == other.first else self.first < other.first
+
+    def __str__(self):
+        return "{} {}".format(self.first, self.last)
+
+
+def eliminate_duplicates(names: List[Name]):
+    names.sort()
+    write_idx = 1
+    for curr_idx in range(1, len(names)):
+        if names[curr_idx] != names[curr_idx - 1]:
+            names[write_idx] = names[curr_idx]
+            write_idx += 1
+
+    del names[write_idx:]
+    return names
+
+
+class Event():
+    def __init__(self, start, finish):
+        self.start = start
+        self.finish = finish
+
+
+class EndPoint():
+    def __init__(self, val, is_start=True):
+        self.val = val
+        self.is_start = is_start
+
+    def __lt__(self, other):
+        if self.val == other.val:
+            return self.is_start
+        else:
+            return self.val < other.val
+
 
 #   13.6 render a calendar
+def find_max_simultaneous_events(events: List[Event]) -> int:
+    # sort all the 2n endpoints
+    endpoints = []
+    for event in events:
+        start = EndPoint(event.start)
+        end = EndPoint(event.finish, False)
+        endpoints.append(start)
+        endpoints.append(end)
+
+    endpoints.sort()
+
+    max_events = 0
+    curr = 0
+    for endpoint in endpoints:
+        if endpoint.is_start:
+            curr += 1
+            max_events = max(max_events, curr)
+        else:
+            curr -= 1
+
+    return max_events
+
+
+class Interval():
+    def __init__(self, left, right, is_left_closed=True, is_right_closed=True):
+        self.left = left
+        self.right = right
+        self.is_left_closed = is_left_closed
+        self.is_right_closed = is_right_closed
+
+    def __lt__(self, other):
+        return self.left < other.left
+
+    def __str__(self):
+        return "{}:{}".format(self.left, self.right)
+
+
+#   13.7 merging intervals
+def add_interval(intervals: List[Interval], new_interval: Interval) \
+        -> List[Interval]:
+    if not intervals:
+        return [new_interval]
+
+    result = []
+    # find all to the left of interval
+    idx = 0
+    while idx < len(intervals):
+        curr = intervals[idx]
+        if (curr.left <= new_interval.left <= curr.right) \
+                or (curr.left <= new_interval.right <= curr.right):
+            break
+        result.append(curr)
+        idx += 1
+
+    new_interval = Interval(min(curr.left, new_interval.left),
+                            max(curr.right, new_interval.right))
+    # keep combining till current is to right of new_interval
+    idx += 1
+    while idx < len(intervals) and new_interval.right > intervals[idx].left:
+        new_interval.right = max(intervals[idx].right, new_interval.right)
+        idx += 1
+
+    result.append(new_interval)
+
+    return result + intervals[idx:]
+
 
 #   13.8 compute the union of intervals
+def union_of_intervals(intervals: List[Interval]):
+    def _is_intersecting(interval_1, interval_2):
+        # wlog assume interval_1 is longer than interval_2
+        if abs(interval_1.left - interval_1.right) < abs(interval_2.left - interval_2.right):
+            interval_1, interval_2 = interval_2, interval_1
+
+        if (interval_1.left <= interval_2.left <= interval_1.right) \
+                or (interval_1.left <= interval_2.right <= interval_1.right):
+            return True
+        else:
+            return False
+
+    if not intervals or len(intervals) == 1:
+        return intervals
+
+    # sort the intervals
+    intervals.sort()
+    result = [intervals[0]]
+    for idx in range(1, len(intervals)):
+        curr = intervals[idx]
+        top = result[-1]
+
+        if _is_intersecting(curr, top):
+            # perform union of curr and top
+            top.left = min(top.left, curr.left)
+            top.right = max(top.right, curr.right)
+        else:
+            result.append(curr)
+
+    return result
+
 
 #   13.9 partitioning and sorting an array with many repeated entries
+def group_by_age(names: List[Tuple]):
+    # create hash-map of age to indices
+    age_to_indices = {}
+    for idx, name in enumerate(names):
+        age = name[1]
+        if age in age_to_indices:
+            indices = age_to_indices[age]
+            indices.add(idx)
+        else:
+            s = set()
+            s.add(idx)
+            age_to_indices[age] = s
+
+    for i in range(0, len(names)):
+        curr = next(iter(age_to_indices))
+        indices = age_to_indices[curr]
+
+        if not indices:
+            continue
+
+        curr_idx = next(iter(indices))
+        if i == curr_idx:
+            indices.remove(curr_idx)
+            continue
+
+        indices.remove(curr_idx)
+
+        # we have to move curr_idx to i
+        elem_at_i = names[i]
+        swap_indices = age_to_indices[elem_at_i[1]]
+
+        # swap the elements at i and curr_idx
+        names[i], names[curr_idx] = names[curr_idx], names[i]
+
+        # change i in swap_indices to curr_idx
+        swap_indices.remove(i)
+        swap_indices.add(curr_idx)
+
+        if not indices:
+            del age_to_indices[curr]
+
 
 #   13.11 implement a fast sorting algorithm for lists
+def stable_sort_linked_list(head: Node):
+    if not head or not head.next:
+        return head
+
+    slow, fast = head, head
+    pre_slow = None
+    while fast and fast.next:
+        pre_slow = slow
+        slow = slow.next
+        fast = fast.next.next
+
+    if pre_slow:
+        pre_slow.next = None
+
+    return merge_sorted_list(stable_sort_linked_list(head),
+                             stable_sort_linked_list(slow))
 
 # todo
 #   13.5 smallest non-constructible value
-#   13.7 merging intervals
 #   13.10 Team Photo Day - 1
--- tests/sorting/epi_sorting_test.py
+++ tests/sorting/epi_sorting_test.py
@@ -1,11 +1,6 @@
 import unittest
-from sorting import Student, \
-    sort_students_by_gpa, \
-    sort_students_by_name, \
-    intersect_two_sorted_lists, \
-    intersect_two_sorted_lists_binary_search, \
-    get_h_index, \
-    merge_sorted_arrays
+from sorting import *
+from linked_list import Node
 
 
 class EpiSortingTestCase(unittest.TestCase):
@@ -71,8 +66,182 @@ class EpiSortingTestCase(unittest.TestCase):
         self.assertListEqual(expected, result)
 
     def test_get_h_index(self):
-        pass
+        citations = [1, 20, 20, 17, 16, 2, 12, 13, 17, 23, 9, 23, 9, 19]
+        h_index = get_h_index(citations)
+        self.assertEqual(10, h_index)
 
+        h_index = get_h_index([3, 44, 38, 23, 37, 43, 28, 21,
+                               12, 44, 0, 37, 13, 23, 28, 2,
+                               41, 26, 35, 1, 42, 28, 10])
+
+        self.assertEqual(16, h_index)
+
+        self.assertEqual(10,
+                         get_h_index([0, 21, 14, 15, 16,
+                                      19, 6, 13, 20, 13,
+                                      24, 15, 7]))
+
+        self.assertEqual(18,
+                         get_h_index([47, 20, 42, 8, 7, 18,
+                                      36, 34, 50, 30, 50, 8,
+                                      8, 3, 22, 42, 34, 42,
+                                      31, 15, 33, 36, 27, 10,
+                                      5, 40]))
+
+    def test_remove_first_name_duplicates(self):
+        names = [
+            Name('Ian', 'Bothom'),
+            Name('David', 'Bell'),
+            Name('Ian', 'Carter'),
+            Name('Jimmy', 'Page'),
+            Name('David', 'Bowie')
+        ]
+
+        names = eliminate_duplicates(names)
+
+        self.assertListEqual(['Bell', 'Bothom', 'Page'], [name.last for name in names])
+
+    def test_find_max_simultaneous_events(self):
+        events = [
+            Event(0, 1),
+            Event(0, 2),
+            Event(2, 4),
+            Event(3, 6),
+            Event(4, 6),
+            Event(6, 8),
+            Event(7, 9),
+            Event(11, 12),
+            Event(14, 17)
+        ]
+        self.assertEqual(3, find_max_simultaneous_events(events))
+
+        events.append(Event(3, 6))
+        self.assertEqual(4, find_max_simultaneous_events(events))
+
+    def test_add_interval(self):
+        intervals = [
+            Interval(-4, -1),
+            Interval(0, 2),
+            Interval(3, 6),
+            Interval(7, 9),
+            Interval(11, 12),
+            Interval(14, 17)
+        ]
+        intervals = add_interval(intervals, Interval(1, 8))
+        expected = [[-4, -1], [0, 9], [11, 12], [14, 17]]
+
+        intervals = [[interval.left, interval.right]
+                     for interval in intervals]
+
+        self.assertListEqual(expected, intervals)
+
+        intervals = [[6, 14], [18, 18], [22, 30], [32, 42], [51, 57], [59, 62], [66, 73], [78, 79], [84, 86], [92, 100],
+                     [108, 118], [119, 120], [123, 126], [130, 136], [139, 144], [152, 154], [160, 169], [173, 177],
+                     [182, 187], [191, 200], [205, 210], [217, 220], [230, 239], [249, 259], [260, 265], [266, 270],
+                     [273, 279], [281, 291], [292, 302], [312, 319], [320, 329], [336, 341], [346, 347], [353, 357],
+                     [362, 369], [371, 378], [382, 388], [392, 395], [403, 412], [420, 427], [432, 440], [445, 447],
+                     [452, 458], [460, 468], [476, 486], [487, 491], [492, 501], [511, 518], [526, 526], [532, 537],
+                     [538, 546]]
+        intervals = [Interval(i[0], i[1]) for i in intervals]
+        intervals = add_interval(intervals, Interval(263, 280))
+        intervals = [[interval.left, interval.right]
+                     for interval in intervals]
+        expected = [[6, 14], [18, 18], [22, 30], [32, 42], [51, 57], [59, 62], [66, 73], [78, 79], [84, 86], [92, 100],
+                    [108, 118], [119, 120], [123, 126], [130, 136], [139, 144], [152, 154], [160, 169], [173, 177],
+                    [182, 187], [191, 200], [205, 210], [217, 220], [230, 239], [249, 259], [260, 280], [281, 291],
+                    [292, 302], [312, 319], [320, 329], [336, 341], [346, 347], [353, 357], [362, 369], [371, 378],
+                    [382, 388], [392, 395], [403, 412], [420, 427], [432, 440], [445, 447], [452, 458], [460, 468],
+                    [476, 486], [487, 491], [492, 501], [511, 518], [526, 526], [532, 537], [538, 546]]
+        self.assertListEqual(expected, intervals)
+
+    def test_union_of_intervals(self):
+        intervals = [[8, True, 9, False], [157, False, 166, False], [163, False, 168, True], [177, True, 182, True],
+                     [30, True, 32, False], [169, False, 170, True], [149, True, 153, False], [121, False, 129, False],
+                     [226, False, 228, False], [211, False, 217, True], [177, True, 184, False], [8, False, 17, True],
+                     [225, True, 233, True], [16, True, 18, False], [243, False, 246, False], [102, True, 110, True],
+                     [170, True, 173, True], [169, True, 175, True], [169, False, 170, True], [105, True, 108, False],
+                     [125, False, 128, False], [61, True, 67, False], [175, False, 180, False], [205, True, 210, False],
+                     [125, False, 128, False]]
+
+        intervals = [Interval(interval[0], interval[2], interval[1], interval[3])
+                     for interval in intervals]
+
+        union = union_of_intervals(intervals)
+
+        expected = [[8, True, 18, False], [30, True, 32, False], [61, True, 67, False], [102, True, 110, True],
+                    [121, False, 129, False], [149, True, 153, False], [157, False, 168, True], [169, True, 184, False],
+                    [205, True, 210, False], [211, False, 217, True], [225, True, 233, True], [243, False, 246, False]]
+
+        for idx, interval in enumerate(union):
+            # curr = [interval.left,
+            #         interval.is_left_closed,
+            #         interval.right,
+            #         interval.is_right_closed]
+            curr = [interval.left,
+                    interval.right]
+            self.assertListEqual([expected[idx][0], expected[idx][2]], curr)
+        # -- -- -- -- -- -- -- --
+        intervals = [[10, 11], [0, 3], [5, 7], [12, 14], [1, 1], [2, 4], [3, 4], [7, 8], [8, 11]]
+
+        intervals = [Interval(interval[0], interval[1])
+                     for interval in intervals]
+
+        union = union_of_intervals(intervals)
+
+        expected = [[0, 4], [5, 11], [12, 14]]
+
+        for idx, interval in enumerate(union):
+            # curr = [interval.left,
+            #         interval.is_left_closed,
+            #         interval.right,
+            #         interval.is_right_closed]
+            curr = [interval.left,
+                    interval.right]
+            self.assertListEqual([expected[idx][0], expected[idx][1]], curr)
+
+    def test_stable_sort_linked_list(self):
+        head = Node(14)
+        curr = head
+        for i in [3, 67, 12, 13, 66, 8]:
+            node = Node(i)
+            curr.next = node
+            curr = node
+
+        ll = stable_sort_linked_list(head)
+
+        actual = []
+        while ll:
+            actual.append(ll.val)
+            ll = ll.next
+
+        self.assertIsNotNone([3, 8, 12, 13, 14, 66, 67], actual)
+
+    def test_group_by_age(self):
+        names = [
+            ('Greg', 14),
+            ('John', 12),
+            ('Andy', 11),
+            ('Jim', 13),
+            ('Phil', 12),
+            ('Bob', 13),
+            ('Chip', 13),
+            ('Tim', 14)
+        ]
+
+        group_by_age(names)
+
+        expected = [
+            ('Greg', 14),
+            ('Tim', 14),
+            ('Phil', 12),
+            ('John', 12),
+            ('Andy', 11),
+            ('Bob', 13),
+            ('Chip', 13),
+            ('Jim', 13)
+        ]
+
+        self.assertListEqual(expected, names)
 
 if __name__ == '__main__':
     unittest.main()
