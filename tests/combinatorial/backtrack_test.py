ac49e1f5b6c8df6c47506d8211c2b8a01e287f5e
Sumit Haswar <sumithaswar@gmail.com>
2021-01-21 22:31:00 -0800

    add permutation algo

--- tests/combinatorial/backtrack_test.py
+++ tests/combinatorial/backtrack_test.py
@@ -7,16 +7,24 @@ class BacktrackTestCase(unittest.TestCase):
         subset = Subsets()
         n = 3
         k = 0
-        a = [False] * n
+        a = [False] * (n + 1)
 
         subset.backtrack(a, k, n)
 
         expected = ['1,2,3', '1,2', '1,3', '1', '2,3', '2', '3', '']
-
         self.assertListEqual(sorted(expected), sorted(subset.result))
 
     def test_permutation(self):
-        pass
+        permutation = Permutation()
+        n = 3
+        k = 0
+        a = [0] * (n + 1)
+
+        permutation.backtrack(a, k, n)
+
+        expected = ['1,2,3', '1,3,2', '2,3,1', '2,1,3', '3,2,1', '3,1,2']
+        self.assertListEqual(sorted(expected), sorted(permutation.result))
+
 
 if __name__ == '__main__':
     unittest.main()
