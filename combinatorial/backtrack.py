fadff93b79a05d3199b797fbe978b80bd5a7226c
Sumit Haswar <sumithaswar@gmail.com>
2021-01-21 22:32:07 -0800

    add permutation algo (#63)

--- combinatorial/backtrack.py
+++ combinatorial/backtrack.py
@@ -2,7 +2,7 @@ from typing import List
 from abc import ABC, abstractmethod
 
 
-class Backtrack():
+class Backtrack(ABC):
 
     def __init__(self):
         self.finished = False
@@ -38,8 +38,8 @@ class Backtrack():
             number_of_candidates = self.construct_candidates(a, k, input_data, candidates)
 
             for i in range(number_of_candidates):
-                a[k - 1] = candidates[i]
-                # a[k] = candidates[i]
+                # a[k - 1] = candidates[i]
+                a[k] = candidates[i]
                 # make_move
                 self.backtrack(a, k, input_data)
                 # unmake_move
@@ -60,18 +60,36 @@ class Subsets(Backtrack):
         return 2
 
     def process_solution(self, a, k, input_data):
-        soln = []
-        for idx in range(0, k):
+        solution = []
+        for idx in range(1, k + 1):
             if a[idx]:
-                soln.append(idx + 1)
+                solution.append(idx)
 
-        self.result.append(','.join(str(x) for x in soln))
+        self.result.append(','.join(str(x) for x in solution))
 
 
 class Permutation(Backtrack):
 
     def is_a_solution(self, a, k, input_data):
-        pass
+        return k == input_data
+
+    def process_solution(self, a, k, input_data):
+        solution = []
+        for idx in range(1, k + 1):
+            solution.append(a[idx])
+
+        self.result.append(','.join(str(x) for x in solution))
 
     def construct_candidates(self, a, k, input_data, candidates):
-        pass
+        in_perm = set()
+
+        # for 1 to k get all numbers which are in permutation
+        for i in range(1, k):
+            in_perm.add(a[i])
+
+        # iter all from 1 to N, adding numbers which are NOT in in_perm
+        for i in range(1, input_data + 1):
+            if i not in in_perm:
+                candidates.append(i)
+
+        return len(candidates)
