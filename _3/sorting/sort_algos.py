f39ef42fdff47a328a2c71e8415544a692d38042
Sumit Haswar <sumithaswar@gmail.com>
2025-04-20 18:26:04 -0700

    _3-sorting-a (#99)

--- _3/sorting/sort_algos.py
+++ _3/sorting/sort_algos.py
@@ -1,1 +1,13 @@
+from typing import List
 
+# heap sort
+def heap_sort(arr: List[int]):
+    pass
+
+# merge sort
+def merge_sort(arr: List[int]):
+    pass
+
+# quick sort
+def quick_sort(arr: List[int]):
+    pass
